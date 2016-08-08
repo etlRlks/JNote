00413abeab6c92d3b4dab25a29c6656976a2edfe
jogue <969318963@qq.com>
2016-08-08 17:32:34 +0800

    splash activity

--- app/src/main/java/com/jogue/jnote/activity/SplashActivity.java
+++ app/src/main/java/com/jogue/jnote/activity/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.jogue.jnote.activity;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by jogue- on 2016/8/8.
  */
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity{
 }
